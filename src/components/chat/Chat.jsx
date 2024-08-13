d6f5e43de3e9ac38599841e82f95ad3524721e38
tdkunz <74592421+tdkunz@users.noreply.github.com>
2024-08-13 18:14:22 +0700

    chat.jsx

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -11,17 +11,12 @@ import {
 import { db } from "../../lib/firebase";
 import { useChatStore } from "../../lib/chatStore";
 import { useUserStore } from "../../lib/userStore";
-import upload from "../../lib/upload";
 import { format } from "timeago.js";
 
 const Chat = ({ onShowDetail, showDetail }) => {
   const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
-  const [img, setImg] = useState({
-    file: null,
-    url: "",
-  });
 
   const { currentUser } = useUserStore();
   const { chatId, user, isCurrentUserBlocked, isReceiverBlocked } =
@@ -39,57 +34,45 @@ const Chat = ({ onShowDetail, showDetail }) => {
     };
   }, [chatId]);
 
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chat]);
+
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
   };
 
-  const handleImg = (e) => {
-    if (e.target.files[0]) {
-      setImg({
-        file: e.target.files[0],
-        url: URL.createObjectURL(e.target.files[0]),
-      });
-    }
-  };
-
   const handleSend = async () => {
     if (text === "") return;
-
-    let imgUrl = null;
-
+  
     try {
-      if (img.file) {
-        imgUrl = await upload(img.file);
-      }
-
       await updateDoc(doc(db, "chats", chatId), {
         messages: arrayUnion({
           senderId: currentUser.id,
           text,
           createdAt: new Date(),
-          ...(imgUrl && { img: imgUrl }),
         }),
       });
-
+  
       const userIDs = [currentUser.id, user.id];
-
+  
       userIDs.forEach(async (id) => {
         const userChatsRef = doc(db, "userchats", id);
         const userChatsSnapshot = await getDoc(userChatsRef);
-
+  
         if (userChatsSnapshot.exists()) {
           const userChatsData = userChatsSnapshot.data();
-
+  
           const chatIndex = userChatsData.chats.findIndex(
             (c) => c.chatId === chatId
           );
-
+  
           userChatsData.chats[chatIndex].lastMessage = text;
           userChatsData.chats[chatIndex].isSeen =
             id === currentUser.id ? true : false;
           userChatsData.chats[chatIndex].updatedAt = Date.now();
-
+  
           await updateDoc(userChatsRef, {
             chats: userChatsData.chats,
           });
@@ -98,12 +81,13 @@ const Chat = ({ onShowDetail, showDetail }) => {
     } catch (err) {
       console.log(err);
     } finally {
-      setImg({
-        file: null,
-        url: "",
-      });
-
       setText("");
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSend();
     }
   };
 
@@ -129,33 +113,14 @@ const Chat = ({ onShowDetail, showDetail }) => {
             key={message?.createAt}
           >
             <div className="texts">
-              {message.img && <img src={message.img} alt="" />}
               <p>{message.text}</p>
               <span>{format(message.createdAt.toDate())}</span>
             </div>
           </div>
         ))}
-        {img.url && (
-          <div className="message own">
-            <div className="texts">
-              <img src={img.url} alt="" />
-            </div>
-          </div>
-        )}
         <div ref={endRef}></div>
       </div>
       <div className="bottom">
-        <div className="icons">
-          <label htmlFor="file">
-            <img src="./img1.png" alt="" />
-          </label>
-          <input
-            type="file"
-            id="file"
-            style={{ display: "none" }}
-            onChange={handleImg}
-          />
-        </div>
         <input
           type="text"
           placeholder={
@@ -165,6 +130,7 @@ const Chat = ({ onShowDetail, showDetail }) => {
           }
           value={text}
           onChange={(e) => setText(e.target.value)}
+          onKeyPress={handleKeyPress}
           disabled={isCurrentUserBlocked || isReceiverBlocked}
         />
         <div className="emoji">
