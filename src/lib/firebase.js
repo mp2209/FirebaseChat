66d10cc26e3dee95ae77e906bd9638a1ee790ffa
Minh <minh2209ak@gmail.com>
2024-08-16 15:32:17 +0700

    Update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,23 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: "reactchat-8953e.firebaseapp.com",
-//   projectId: "reactchat-8953e",
-//   storageBucket: "reactchat-8953e.appspot.com",
-//   messagingSenderId: "989490756392",
-//   appId: "1:989490756392:web:9e54e92c7b39c531b82e1d"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// export const auth = getAuth()
-// export const db = getFirestore()
-// export const storage = getStorage()
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -30,7 +10,7 @@ const firebaseConfig = {
   storageBucket: "tesst2-26683.appspot.com",
   messagingSenderId: "714110152662",
   appId: "1:714110152662:web:2d00038d63aab2eb5cfe77",
-  measurementId: "G-CFV4C8L10K"
+  measurementId: "G-CFV4C8L10K",
 };
 
 const app = initializeApp(firebaseConfig);
